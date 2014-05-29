3a6d543396bc9ed88af91fe9c9960c03810a4845
Rich <Rich@169.254.73.16>
2014-05-29 06:26:28 -0500

    adding updates

---
+++ drools_6.0_test/src/main/java/bank/service/impl/BankingInquiryServiceImpl.java
@@ -0,0 +1,14 @@
+package bank.service.impl;
+
+import bank.model.Account;
+import bank.service.BankingInquiryService;
+
+public class BankingInquiryServiceImpl implements BankingInquiryService {
+
+	@Override
+	public boolean isAccountNumberUnique(Account account) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
--- drools_6.0_test/src/main/java/junit/ValidationTest.java
+++ drools_6.0_test/src/main/java/junit/ValidationTest.java
@@ -1,28 +1,28 @@
 package junit;
 
-import org.drools.core.marshalling.impl.ProtobufMessages.KnowledgeBase;
 import org.junit.BeforeClass;
 import org.kie.api.KieBaseConfiguration;
 import org.kie.api.KieServices;
 import org.kie.api.builder.KieBuilder;
 import org.kie.api.builder.KieFileSystem;
+import org.kie.api.builder.Message;
 import org.kie.api.builder.model.KieBaseModel;
 import org.kie.api.builder.model.KieModuleModel;
 import org.kie.api.conf.EqualityBehaviorOption;
 import org.kie.api.conf.EventProcessingOption;
 import org.kie.api.io.ResourceType;
 import org.kie.api.runtime.KieContainer;
-import org.kie.api.runtime.StatelessKieSession;
-import org.kie.internal.KnowledgeBaseFactory;
 import org.kie.internal.conf.SequentialOption;
-import org.kie.internal.io.ResourceFactory;
 
+import bank.service.BankingInquiryService;
+import bank.service.impl.BankingInquiryServiceImpl;
 import bank.validation.ReportFactory;
+import bank.validation.impl.BankingValidationServiceImpl;
 
 public class ValidationTest {
 	static KieServices kServices = KieServices.Factory.get();
-	static KieContainer kc = kServices.getKieClasspathContainer();
-	static StatelessKieSession ksession = kc.newStatelessKieSession();
+	//static KieContainer kc = kServices.getKieClasspathContainer();
+	//static StatelessKieSession ksession = kc.newStatelessKieSession();
 	
 	static KieModuleModel kModuleModel = kServices.newKieModuleModel();
 	
@@ -48,10 +48,20 @@ public class ValidationTest {
 
 		KieBuilder builder = kServices.newKieBuilder(kFileSystem).buildAll();
 		
+		KieContainer kieContainer = kServices.newKieContainer(kServices.getRepository().getDefaultReleaseId());
+		
+		assertEquals(0,builder.getResults().getMessages(Message.Level.ERROR).size());
 		//if(builder.hasErrors()){
 			//throw new RuntimeException(builder.getErrors().toString());
 		//}
 		
+		BankingInquiryService inquiryService = new BankingInquiryServiceImpl();
+		reportFactory = new DefaultReportFactory();
+	}
+
+	private static void assertEquals(int i, int size) {
+		// TODO Auto-generated method stub
+		System.out.println(i == size);
 	}
 
 }
